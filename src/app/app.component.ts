1440e2ae1d0973e3fd2110d01d900edc4b34273c
Thuoq <thuonghamno3xz@gmail.com>
2022-06-21 21:10:58 +0700

    done day 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: './app.component.html', // Inline template
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular100-d-o-c';
+  name = 'angular100-d-o-c';
+
+  // Data binding
+  inputType = 'text';
+  user = {
+    name: 'thuong'
+  };
+
+  //  Event binding
+
+  handler(): void {
+    console.log('Click');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 import {HelloComponent} from './hello/hello.component';
-import { HiComponent } from './hi/hi.component';
+import {HiComponent} from './hi/hi.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [ // this place phân bổ cho cái app module
     AppComponent,
     HelloComponent,
-    HiComponent
+    HiComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
