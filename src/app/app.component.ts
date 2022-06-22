4fae11f2d474b16d7aa48d7ef8a2fe3ae7ba39f6
Thuoq <thuonghamno3xz@gmail.com>
2022-06-22 22:23:04 +0700

    done day 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular100-d-o-c';
+  isDanger = false;
+  isWarning = false;
+  classes = `box ${this.isDanger ? 'red-border' : ''} yellow-background`;
+
+  toggleDanger(): void {
+    this.isDanger = !this.isDanger;
+    this.classes = `box ${this.isDanger ? 'red-border' : ''} yellow-background`;
+  }
 }
