0d2bd3330a1e4524f960a6445134947fe9f7c119
Thuoq <thuonghamno3xz@gmail.com>
2022-06-22 14:43:40 +0700

    add ngfor day 5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,38 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular100-d-o-c';
+  users = [
+    {
+      id: 1,
+      firstName: 'Flora',
+      lastName: 'Twell',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+    {
+      id: 2,
+      firstName: 'Priscella',
+      lastName: 'Signe',
+      email: 'psigne1@berkeley.edu',
+      gender: 'Female',
+      ipAddress: '183.243.228.65',
+    },
+    {
+      id: 3,
+      firstName: 'Pertrol',
+      lastName: 'Limeax',
+      email: 'ftwell0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.120.237.33',
+    },
+    {
+      id: 4,
+      firstName: 'FPT',
+      lastName: 'Software',
+      email: 'fpt0@phoca.cz',
+      gender: 'Female',
+      ipAddress: '99.180.237.33',
+    },
+  ];
 }
